18ce11353871560fe0af06271a10d80efdc47f77
BAPATLA SRAVAN <103932090+bapatlasravan@users.noreply.github.com>
2022-07-18 13:59:37 +0530

    Add files via upload

---
+++ Sum of (N) no of odd numbers.py
@@ -0,0 +1,21 @@
+                                    #while loop
+"""
+n=int(input("enter the number upto where the we want the sum of odd numbers: "))
+num=1
+sum=0
+while num<= n:
+    sum=sum+num
+    num+=2
+print("The sum of odd numbers is",sum)
+"""
+                                    #for loop
+
+""""
+n=int(input("enter the number upto where the we want the sum of odd numbers: "))
+num=1
+sum=1
+for i in range (-1,n,2):
+    sum=sum+i
+print("The sum of odd numbers is",sum)
+"""
+
